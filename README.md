b58a725170df95152b828fe02859e3b1227dfef6
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-04-30 21:50:16 +0200

    Updart

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wp-endscreen-posts
-Select random posts containing videos
+Select random posts containing videos special take.
+
+Might expand it to the future for detecting new posts containing a video file when I have time...
+
--- wp-endscreen-posts/wp-endscreen-posts.php
+++ wp-endscreen-posts/wp-endscreen-posts.php
@@ -27,16 +27,22 @@ function wp_endscreen_posts() {
   if ($query->have_posts()) {
     echo '<div id="endscreen-data">';
     echo '<div id="endscreen-container">';
+    $i = 0;
+    $class_name = '';
     while ($query->have_posts()) {
       $query->the_post();
-      echo '<div class="endscreen-item"><a href="' . get_the_permalink() .'">
+      if($i == 4){
+        $class_name=" endscreen-small";
+      }
+      echo '<div class="endscreen-item '.$class_name.'"><a href="' . get_the_permalink() .'">
         <img src="'. get_the_post_thumbnail_url('', 'video-thumb') .'"></a>
         <div class="endscreen-item-title">' . get_the_title() .'</div></div>
 ';
-    }
+  $i++;  
+  }
     echo '</div>';
     echo '</div>';
   }
 }
 
-add_shortcode('[wp-endscreen]', 'wp_endscreen_posts');
+add_shortcode('wp-endscreen', 'wp_endscreen_posts');
